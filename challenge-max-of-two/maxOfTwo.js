9a7fa1c95225e376f5b082cab1fa563fd0e94fb5
Daniel Haycraft <111464698+daniel-haycraft@users.noreply.github.com>
2022-08-24 01:24:07 +0000

    Initial commit

---
+++ challenge-max-of-two/maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (x > y){
+  console.log("x is bigger than y")
+} else if (x < y) {
+  console.log("y is bigger than x")
+} else {
+  console.log("x is the same and y")
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+let x = 50
+let y = 50
+
+// Write your code below this line.
+
+if (x > y){
+  console.log("x is bigger than y")
+} else if (x < y) {
+  console.log("y is bigger than x")
+} else {
+  console.log("x is the same and y")
+}
