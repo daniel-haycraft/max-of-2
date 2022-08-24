4ba3dec772038a9e0393c99820fcf46809920284
Daniel Haycraft <111464698+daniel-haycraft@users.noreply.github.com>
2022-08-24 01:29:42 +0000

    last draft

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let x = 50
+let x = 52
 let y = 50
 
 // Write your code below this line.
 
 if (x > y){
-  console.log("x is bigger than y")
-} else if (x < y) {
-  console.log("y is bigger than x")
+  console.log(`${x}`)
+} else if (y > x) {
+  console.log(`${y}`)
 } else {
-  console.log("x is the same and y")
+  console.log(x, y)
 }
